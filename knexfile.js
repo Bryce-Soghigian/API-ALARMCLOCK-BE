f356cc66179e2635f614b8a4ad06b2857167bbe6
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-29 09:51:11 -0600

    fixing production knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgres',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations'
