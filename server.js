fbca853151678b37f47f136c65b421983b86e2d4
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-04 15:05:02 -0600

    working server does cool things

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const axios = require('axios')
 const URLROUTER = require('./Router/url-router')
 
 
@@ -17,6 +18,39 @@ server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
 
+//========================================================
+
+let arr = [];
+const getOnInterval = () => {
+    arr=[]
+    axios.get("https://api-alarm-clock.herokuapp.com/url/urls")
+    .then((res) => {
+
+        arr.push(res.data)
+        console.log(arr,"i am arr in getOnInterval")
+    }).catch((err) => {
+        console.error(err)
+    })
+    for(let i = 0;i<arr.length;I++){
+        arr.push(arr[i])
+    }
+}
+getOnInterval()
+
+    const PingOnInterval = () => {
+        for(let i = 0; i< arr.length;i++){
+            axios.get(arr[0][i]["link"])
+            .then((res) => {
+                console.log(arr[i])
+                console.log(res.data)
+            }).catch((err) => {
+                console.error(err)
+            })
+        }
+    console.log(arr,"i am array in PingInterval")
+    // console.log(arr[0])
+    }
+
 //==============Endpoints && Routers========================//
 server.get("/", (req,res) => {
     res.json({"message":"Hey i am alive"})
@@ -24,3 +58,10 @@ server.get("/", (req,res) => {
 
 server.use('/url',URLROUTER)
 
+
+//=============Time=====================================//
+setInterval(getOnInterval,100000)
+setImmediate((arg) => {
+
+setInterval(PingOnInterval, 174000);
+})
